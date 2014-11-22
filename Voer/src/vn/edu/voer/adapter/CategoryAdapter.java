ba8d04ba55dceb6dd7e04051249f8d8c580a57e5
Nguyen Tien Hoang <hoangnt1@vn.ibm.com>
2014-11-22 23:06:43 +0700

    create category screen

--- Voer/src/vn/edu/voer/adapter/CategoryAdapter.java
+++ Voer/src/vn/edu/voer/adapter/CategoryAdapter.java
@@ -3,7 +3,7 @@ package vn.edu.voer.adapter;
 import java.util.List;
 
 import vn.edu.voer.R;
-import vn.edu.voer.object.Book;
+import vn.edu.voer.object.Category;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.view.LayoutInflater;
@@ -12,18 +12,18 @@ import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.TextView;
 
-public class BookAdapter extends BaseAdapter {
-	private List<Book> listBooks;
+public class CategoryAdapter extends BaseAdapter {
+	private List<Category> listCategories;
 	private LayoutInflater mInflater = null;
 
-	public BookAdapter(Context context, List<Book> listBooks) {
-		this.listBooks = listBooks;
+	public CategoryAdapter(Context context, List<Category> listCategories) {
+		this.listCategories = listCategories;
 		mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
 	}
 
 	@Override
 	public int getCount() {
-		return listBooks.size();
+		return listCategories.size();
 	}
 
 	@Override
@@ -33,7 +33,7 @@ public class BookAdapter extends BaseAdapter {
 
 	@Override
 	public Object getItem(int position) {
-		return listBooks.get(position);
+		return listCategories.get(position);
 	}
 
 	@SuppressLint("InflateParams")
@@ -41,16 +41,16 @@ public class BookAdapter extends BaseAdapter {
 	public View getView(int position, View convertView, ViewGroup parent) {
 		ViewHolder holder;
 		if (convertView == null) {
-			convertView = mInflater.inflate(R.layout.layout_item_book, null);
+			convertView = mInflater.inflate(R.layout.layout_item_category, null);
 			holder = new ViewHolder();
 			convertView.setTag(holder);
 		} else {
 			holder = (ViewHolder) convertView.getTag();
 		}
 
-		Book book = listBooks.get(position);
-		if (book != null) {
-			holder.lblBookName = detail(convertView, R.id.lblBookName, book.getName());
+		Category category = listCategories.get(position);
+		if (category != null) {
+			holder.lblCategoryName = detail(convertView, R.id.lblCategoryName, category.getName());
 		}
 
 		return convertView;
@@ -63,6 +63,6 @@ public class BookAdapter extends BaseAdapter {
 	}
 
 	static class ViewHolder {
-		TextView lblBookName;
+		TextView lblCategoryName;
 	}
 }
--- Voer/src/vn/edu/voer/object/Category.java
+++ Voer/src/vn/edu/voer/object/Category.java
@@ -5,33 +5,44 @@ public class Category {
 	private String name;
 	private int parent;
 	private String description;
-	
+
+	public Category() {
+	}
+
+	public Category(int id, String name, int parent, String description) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.parent = parent;
+		this.description = description;
+	}
+
 	/**
 	 * @return the id
 	 */
 	public int getId() {
 		return id;
 	}
-	
+
 	/**
 	 * @return the name
 	 */
 	public String getName() {
 		return name;
 	}
-	
+
 	/**
 	 * @return the parent
 	 */
 	public int getParent() {
 		return parent;
 	}
-	
+
 	/**
 	 * @return the description
 	 */
 	public String getDescription() {
 		return description;
 	}
-	
+
 }
